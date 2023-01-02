5ca308e8d5d4bd5cca423d23f30d81bc5196c18c
kenanxasiyevv <kananakh@code.edu.az>
2023-01-03 01:27:44 +0400

    Add project files.

---
+++ EvTapshirigi/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EvTapshirigi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -2, -3, 9, 10 };
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < arr.Length ; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    sum += arr[i];
+                    count++;
+                }
+            }
+            Console.Write($"cem {sum} say {count}");
+        }
+       
+    }
+}
